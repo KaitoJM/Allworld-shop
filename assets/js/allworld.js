499c265ffa6a1c41a86b89976aed79256af45ad5
JM Mac <johnmarkmancol@gmail.com>
2025-02-15 13:58:27 +0800

    fix: put trim on address and city input

--- assets/js/allworld.js
+++ assets/js/allworld.js
@@ -1,7 +1,7 @@
-jQuery(function() {
-  $('#image-slider').owlCarousel({
-    loop:true,
-    margin:10,
+jQuery(function () {
+  $("#image-slider").owlCarousel({
+    loop: true,
+    margin: 10,
     items: 1,
     dots: true,
     autoplay: true,
@@ -9,11 +9,11 @@ jQuery(function() {
     fluidSpeed: true,
     smartSpeed: 1000,
     autoplayHoverPause: true,
-    animateOut: 'fadeOut'
-  })
-  $('#image-slider1').owlCarousel({
-    loop:true,
-    margin:10,
+    animateOut: "fadeOut",
+  });
+  $("#image-slider1").owlCarousel({
+    loop: true,
+    margin: 10,
     items: 1,
     dots: false,
     autoplay: true,
@@ -21,11 +21,11 @@ jQuery(function() {
     fluidSpeed: true,
     smartSpeed: 1000,
     autoplayHoverPause: true,
-    animateOut: 'fadeOut'
-  })
-  $('#image-slider2').owlCarousel({
-    loop:true,
-    margin:10,
+    animateOut: "fadeOut",
+  });
+  $("#image-slider2").owlCarousel({
+    loop: true,
+    margin: 10,
     items: 1,
     dots: false,
     autoplay: true,
@@ -33,49 +33,49 @@ jQuery(function() {
     fluidSpeed: true,
     smartSpeed: 1000,
     autoplayHoverPause: true,
-    animateOut: 'fadeOut'
-  })
-  $(window).on('load', function() {
+    animateOut: "fadeOut",
+  });
+  $(window).on("load", function () {
     setTimeout(() => {
-      $('#image-slider1').trigger('refresh.owl.carousel')
-      $('#image-slider2').trigger('refresh.owl.carousel')
+      $("#image-slider1").trigger("refresh.owl.carousel");
+      $("#image-slider2").trigger("refresh.owl.carousel");
     }, 50);
   });
-  $('.product-images-carousel').owlCarousel({
-    loop:true,
-    margin:10,
+  $(".product-images-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
     items: 4,
     dots: true,
     autoplay: true,
     slideSpeed: 1000,
     fluidSpeed: true,
     smartSpeed: 1000,
-    autoplayHoverPause: true
-  })
-  $('.brand-carousel').owlCarousel({
-    loop:true,
-    margin:30,
+    autoplayHoverPause: true,
+  });
+  $(".brand-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
     items: 6,
     autoplay: true,
     slideSpeed: 1000,
     fluidSpeed: true,
     smartSpeed: 1000,
     autoplayHoverPause: true,
-    responsive : {
-      0 : {
+    responsive: {
+      0: {
         items: 4,
       },
-      768 : {
+      768: {
         items: 5,
       },
-      1040 : {
+      1040: {
         items: 6,
-      }
-    }
+      },
+    },
   });
-  $('.testimonials-carousel').owlCarousel({
-    loop:true,
-    margin:0,
+  $(".testimonials-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
     items: 1,
     dots: true,
     autoplay: true,
@@ -83,173 +83,193 @@ jQuery(function() {
     fluidSpeed: true,
     smartSpeed: 1000,
     autoplayHoverPause: true,
-    animateOut: 'fadeOut'
-  })
+    animateOut: "fadeOut",
+  });
 
-  $('.cover-item').zoom();
+  $(".cover-item").zoom();
 
   function initProductCoverCarousel() {
-    const product_image_cover_carousel = $('.product-cover-carousel').owlCarousel({
-      loop:false,
-      margin:30,
+    const product_image_cover_carousel = $(
+      ".product-cover-carousel"
+    ).owlCarousel({
+      loop: false,
+      margin: 30,
       items: 1,
       autoplay: false,
       slideSpeed: 1000,
       fluidSpeed: true,
       smartSpeed: 1000,
-      dotsContainer: '.product-images'
-    })
-  
-    $('.thumb-container').click(function () {
-      product_image_cover_carousel.trigger('to.owl.carousel', [$(this).index(), 500]);
+      dotsContainer: ".product-images",
+    });
+
+    $(".thumb-container").click(function () {
+      product_image_cover_carousel.trigger("to.owl.carousel", [
+        $(this).index(),
+        500,
+      ]);
     });
   }
 
   initProductCoverCarousel();
 
-  prestashop.on('updatedProduct', function() {
+  prestashop.on("updatedProduct", function () {
     initProductCoverCarousel();
-  })
+  });
 
-  prestashop.on('updateCart', function() {
-    if (localStorage.getItem('directToCheckOut')) {
-      localStorage.removeItem('directToCheckOut');
+  prestashop.on("updateCart", function () {
+    if (localStorage.getItem("directToCheckOut")) {
+      localStorage.removeItem("directToCheckOut");
       location.href = prestashop.urls.pages.order;
     }
-  })
+  });
 
-  $(document).on('change', '.payment-method-chck', function() {
-    $('.payment-option-item').removeClass('selected');
-    if ($(this).prop('checked')) {
-      $(this).closest('.payment-option-item').addClass('selected');
+  $(document).on("change", ".payment-method-chck", function () {
+    $(".payment-option-item").removeClass("selected");
+    if ($(this).prop("checked")) {
+      $(this).closest(".payment-option-item").addClass("selected");
     }
-  })
+  });
 
-  $(document).on('click', '.tab-filter-status', function(e) {
-    $('.order-table tbody tr').hide();
-    $('.orders-tab li').removeClass('active')
-    $(this).closest('li').addClass('active')
+  $(document).on("click", ".tab-filter-status", function (e) {
+    $(".order-table tbody tr").hide();
+    $(".orders-tab li").removeClass("active");
+    $(this).closest("li").addClass("active");
 
-    if ($(this).hasClass('tab-all')) {
-      $('.order-table tbody tr').show();
+    if ($(this).hasClass("tab-all")) {
+      $(".order-table tbody tr").show();
     }
 
-    if ($(this).hasClass('tab-to-pay')) {
+    if ($(this).hasClass("tab-to-pay")) {
       $('.order-table tbody tr[data-paid="0"]').show();
     }
 
-    if ($(this).hasClass('tab-to-ship')) {
+    if ($(this).hasClass("tab-to-ship")) {
       $('.order-table tbody tr[data-paid="1"][data-shipped="0"]').show();
     }
 
-    if ($(this).hasClass('tab-to-receive')) {
-      $('.order-table tbody tr[data-paid="1"][data-shipped="1"][data-id_order_state!="5"]').show();
+    if ($(this).hasClass("tab-to-receive")) {
+      $(
+        '.order-table tbody tr[data-paid="1"][data-shipped="1"][data-id_order_state!="5"]'
+      ).show();
     }
 
-    if ($(this).hasClass('tab-completed')) {
+    if ($(this).hasClass("tab-completed")) {
       $('.order-table tbody tr[data-id_order_state="5"]').show();
     }
 
     e.preventDefault();
   });
 
-  $(document).on('click', '.product-details-container .wish-list-trigger', function(e) {
-    $(this).closest('.product-details-container').find('.wishlist-button-add').trigger('click');
-    prestashop.emit('updatedWishlist');
-    e.preventDefault()
-  });
+  $(document).on(
+    "click",
+    ".product-details-container .wish-list-trigger",
+    function (e) {
+      $(this)
+        .closest(".product-details-container")
+        .find(".wishlist-button-add")
+        .trigger("click");
+      prestashop.emit("updatedWishlist");
+      e.preventDefault();
+    }
+  );
 
-  $(document).on('click', '.add-to-whish-list', function(e) {
-    $(this).closest('.product').find('.wishlist-button-add').trigger('click');
-    prestashop.emit('updatedWishlist');
-    e.preventDefault()
+  $(document).on("click", ".add-to-whish-list", function (e) {
+    $(this).closest(".product").find(".wishlist-button-add").trigger("click");
+    prestashop.emit("updatedWishlist");
+    e.preventDefault();
   });
 
   updateWishlistCount();
 
   function updateWishlistCount() {
-    $.get(prestashop.urls.base_url + 'module/blockwishlist/action?action=getAllWishlist', null, function(data) {
-      let count = 0;
-      if (data) {
-        const favorite = JSON.parse(data);
-    
-        favorite.wishlists.forEach((item, k) => {
-          count += parseInt(item.nbProducts) || 0
-        });
+    $.get(
+      prestashop.urls.base_url +
+        "module/blockwishlist/action?action=getAllWishlist",
+      null,
+      function (data) {
+        let count = 0;
+        if (data) {
+          const favorite = JSON.parse(data);
+
+          favorite.wishlists.forEach((item, k) => {
+            count += parseInt(item.nbProducts) || 0;
+          });
+        }
+
+        if (count) {
+          $(".link-fav").append('<span class="count">' + count + "</span>");
+        }
       }
-    
-      if (count) {
-        $('.link-fav').append('<span class="count">' + count + '</span>')
-      }
-    })
+    );
   }
 
-  $(document).on('click', '.link-categories', function(e) {
-    $('.custom-main-menu').css({zIndex: '999'}).show();
-    setTimeout(function() {
-      $('.custom-main-menu').addClass('open');
+  $(document).on("click", ".link-categories", function (e) {
+    $(".custom-main-menu").css({ zIndex: "999" }).show();
+    setTimeout(function () {
+      $(".custom-main-menu").addClass("open");
     }, 100);
     e.preventDefault();
-  })
-  $(document).on('click', '.category-close', function(e) {
-    $('.custom-main-menu').removeClass('open');
-    setTimeout(function() {
-      $('.custom-main-menu').hide().css({zIndex: '-9'});
+  });
+  $(document).on("click", ".category-close", function (e) {
+    $(".custom-main-menu").removeClass("open");
+    setTimeout(function () {
+      $(".custom-main-menu").hide().css({ zIndex: "-9" });
     }, 300);
     e.preventDefault();
-  })
-
-})
+  });
+});
 
 // newsletter dialog
 let dialog_flg = localStorage.getItem("show_email_newsletter_dialog");
-if (dialog_flg != 'hidden') {
-  setTimeout(function() {
-    console.log('triggered shown')
-    $('.newsletter-dialog-wrapper').fadeIn();
-  }, 5000)
+if (dialog_flg != "hidden") {
+  setTimeout(function () {
+    console.log("triggered shown");
+    $(".newsletter-dialog-wrapper").fadeIn();
+  }, 5000);
 } else {
-  console.log('triggered hidden', dialog_flg)
+  console.log("triggered hidden", dialog_flg);
 }
 
-$(document).on('click', '.close-newsletter', function(e) {
-  localStorage.setItem("show_email_newsletter_dialog", 'hidden');
-  $('.newsletter-dialog-wrapper').fadeOut();
-  e.preventDefault()
-})
-
-$(document).on('click', '.buy-now', function(e) {
-  $(this).closest('.add').find('.add-to-cart').trigger('click');
-  localStorage.setItem('directToCheckOut', true);
+$(document).on("click", ".close-newsletter", function (e) {
+  localStorage.setItem("show_email_newsletter_dialog", "hidden");
+  $(".newsletter-dialog-wrapper").fadeOut();
   e.preventDefault();
-})
+});
 
-$(document).on('click', '.product-buy-now', function(e) {
-  $(this).closest('.display-hover').find('.product-add-to-cart').trigger('click');
-  localStorage.setItem('directToCheckOut', true);
+$(document).on("click", ".buy-now", function (e) {
+  $(this).closest(".add").find(".add-to-cart").trigger("click");
+  localStorage.setItem("directToCheckOut", true);
   e.preventDefault();
-})
+});
 
-$(document).on('click', '.sub-menu-img', function(e) {
-  location.href = $(this).next().attr('href')
-})
+$(document).on("click", ".product-buy-now", function (e) {
+  $(this)
+    .closest(".display-hover")
+    .find(".product-add-to-cart")
+    .trigger("click");
+  localStorage.setItem("directToCheckOut", true);
+  e.preventDefault();
+});
 
-$(document).on('click', '.toggle-expand', function(e) {
-  if ($(this).closest('li').find(".sub")[0]) {
-    let sub = $(this).closest('li').find(".sub")[0];
-    
-    if (sub && $(sub).is(':visible')) {
+$(document).on("click", ".sub-menu-img", function (e) {
+  location.href = $(this).next().attr("href");
+});
+
+$(document).on("click", ".toggle-expand", function (e) {
+  if ($(this).closest("li").find(".sub")[0]) {
+    let sub = $(this).closest("li").find(".sub")[0];
+
+    if (sub && $(sub).is(":visible")) {
       $(sub).hide();
-      $(this).find('.fi').removeClass('fi-br-minus').addClass('fi-br-plus');
+      $(this).find(".fi").removeClass("fi-br-minus").addClass("fi-br-plus");
     } else {
       $(sub).show();
-      $(this).find('.fi').removeClass('fi-br-plus').addClass('fi-br-minus');
+      $(this).find(".fi").removeClass("fi-br-plus").addClass("fi-br-minus");
     }
   }
-  
-  e.preventDefault();
-})
 
+  e.preventDefault();
+});
 
 // $(document).on('click', '.add-to-whish-list', function() {
 //   // Get the product ID and customer ID
@@ -275,3 +295,11 @@ $(document).on('click', '.toggle-expand', function(e) {
 //       }
 //   });
 // })
+
+$("#field-address1").change(function (e) {
+  $("#field-address1").val($("#field-address1").val().trim());
+});
+
+$("#field-city").change(function (e) {
+  $("#field-city").val($("#field-city").val().trim());
+});
